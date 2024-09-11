b2c1bd6e6938abe3e3f37d9f526ba81794b37dbf
Amin Shazrin <aminshazrin@yahoo.com>
2024-09-11 11:20:32 +0800

    Add site title dynamically to footer. So that client wouldn't complaint if they are going to change site name.

--- web/app/themes/flexor/resources/views/partials/copyright.blade.php
+++ web/app/themes/flexor/resources/views/partials/copyright.blade.php
@@ -1,7 +1,7 @@
 <div class="container copyright mt-4 text-start">
     <div class="row">
         <div class="col-lg">
-            <p class="disclaimer">Institut Latihan Dewan Bandaraya Kuala Lumpur tidak bertanggungjawab terhadap sebarang
+            <p class="disclaimer">{!! get_bloginfo('name') !!} tidak bertanggungjawab terhadap sebarang
                 kehilangan atau kerosakan yang dialami kerana menggunakan maklumat dalam laman ini</p>
         </div>
     </div>
